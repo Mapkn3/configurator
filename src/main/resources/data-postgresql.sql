c0d878fb2ed1af0bf63d1aab62429aee94e3709a
Mapkn3 <79608236848@ya.ru>
2018-12-05 00:17:47 +0400

    Fix data-postgresql.sql

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -1,23 +1,23 @@
 MERGE INTO currency_table c
 USING (VALUES (0, 'RUR', '₽')) v
 ON v.column1 = c.id
-WHEN NOT MATCHED
+WHEN NOT MATCHED THEN
 INSERT VALUES (v.column1, v.column2, v.column3)
-WHEN MATCHED
-UPDATE SET name = v.column2;
+WHEN MATCHED THEN
+DO NOTHING;
 
 MERGE INTO currency_table c
 USING (VALUES (1, 'USD', '$')) v
 ON v.column1 = c.id
-WHEN NOT MATCHED
+WHEN NOT MATCHED THEN
 INSERT VALUES (v.column1, v.column2, v.column3)
-WHEN MATCHED
-UPDATE SET name = v.column2;
+WHEN MATCHED THEN
+DO NOTHING;
 
 MERGE INTO currency_table c
 USING (VALUES (2, 'EUR', '€')) v
 ON v.column1 = c.id
-WHEN NOT MATCHED
+WHEN NOT MATCHED THEN
 INSERT VALUES (v.column1, v.column2, v.column3)
-WHEN MATCHED
-UPDATE SET name = v.column2;
+WHEN MATCHED THEN
+DO NOTHING;
